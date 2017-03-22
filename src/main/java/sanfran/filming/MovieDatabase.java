421e34e7719a5a8c8813be12e25fbfa3e7e2cfc0
Ari Ayvazyan <ariay@live.at>
2017-03-22 18:45:22 +0100

    read the data from a json file

--- src/main/java/sanfran/filming/MovieDatabase.java
+++ src/main/java/sanfran/filming/MovieDatabase.java
@@ -1,5 +1,10 @@
 package sanfran.filming;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 import java.util.LinkedList;
@@ -17,12 +22,30 @@ public class MovieDatabase {
         this.movieLocations = fetchMovieLocations();
     }
 
+    /**
+     * Retrieves Movie titles with their filming locations
+     *
+     * @return returns null if the movie locations can not be fetched
+     */
     private List<MovieLocation> fetchMovieLocations() {
-        List<MovieLocation> result = new LinkedList<>();
-        result.add(new MovieLocation("Woah", "sample description"));
-        result.add(new MovieLocation("Woah", "sample description"));
-        result.add(new MovieLocation("Woah", "sample description"));
-        return result;
+        try {
+            List<MovieLocation> result = new LinkedList<>();
+            Resource resource = new ClassPathResource("movieLocationData.json");
+            DocumentContext document = JsonPath.parse(resource.getFile());
+
+            JSONArray data = document.read("$.data");
+            for (Object movieRaw : data) {
+                JSONArray movie = (JSONArray) movieRaw;
+                String movieTitle = (String) movie.get(8);
+                String locations = (String) movie.get(10);
+                result.add(new MovieLocation(movieTitle, locations));
+            }
+
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public List<MovieLocation> getAllMovieLocations() {
